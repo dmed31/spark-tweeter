adf228eaef92f8eb9b3456824d91234256a8337f
dmed31 <dhatrim@seas.upenn.edu>
2022-03-20 13:59:53 -0400

    site files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dhatri/Documents/college/year2/clubs/blue-django-starter/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
